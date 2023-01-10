3ed5950345c65c70e8a3bb334f319d9260541da6
Saehwa95 <tpghk1002@naver.com>
2023-01-10 23:20:04 +0900

    :sparkles: 검색 컴포넌트 제작 및 kakao 패키지 설치

---
+++ src/components/main/Search.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import KakaoMap from "./KakaoMap";
+
+const Search = () => {
+  const [inputText, setInputText] = useState("");
+  const [place, setPlace] = useState("");
+
+  const onChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setPlace(inputText);
+    setInputText("");
+  };
+  return (
+    <div>
+      <form className="inputForm" onSubmit={handleSubmit}>
+        <input
+          placeholder="Search Place..."
+          onChange={onChange}
+          value={inputText}
+        />
+        <button type="submit">검색</button>
+      </form>
+      <KakaoMap searchPlace={place}/>
+    </div>
+  );
+};
+
+export default Search;
