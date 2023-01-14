d6552ce1a543f92545196fa5850305bada63b971
정창원 <8500jcw@gmail.com>
2023-01-14 14:26:59 +0900

    :sparkles: feat: 애플리케이션탭에 백 토큰띄우기 위한 프록시설정, 분기처리

--- src/redux/api/instance.js
+++ src/redux/api/instance.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 import { Cookies } from "react-cookie";
 
+const ENV = process.env.NODE_ENV === "development";
+//얘 죽이지 말고 나중에 배포한담에 콘솔 확인해보자. 보면 product로 나올거임 (∵분기처리해서)
+console.log(process.env.NODE_ENV);
+
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_SERVER,
+  baseURL: ENV ? "/" : process.env.REACT_APP_SERVER,
+  withCredentials: true,
 });
 
 const cookie = new Cookies();
