911d41377f9a7431c1a364ad6c0cd210c58f5503
정창원 <8500jcw@gmail.com>
2023-01-20 03:43:25 +0900

    :fire: docs: 업데이트 페이지 삭제, 모달창으로 전환

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -6,7 +6,6 @@ import PostList from "../pages/sns/PostList";
 import Chat from "../pages/socket/Chat";
 import SignUp from "../pages/user/SignUp";
 import CreatePost from "../pages/sns/CreatePost";
-import UpdatePost from "../pages/sns/UpdatePost";
 import Login from "../pages/user/Login";
 import DetailPost from "../pages/sns/DetailPost";
 import Weather from "../pages/main/Weather";
@@ -20,7 +19,6 @@ const Router = () => {
         <Route path="/postlist" element={<PostList />} />
         <Route path="/detail/:postId" element={<DetailPost />} />
         <Route path="/create" element={<CreatePost />} />
-        <Route path="/update" element={<UpdatePost />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/signup" element={<SignUp />} />
