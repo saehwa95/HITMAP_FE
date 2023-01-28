72afff24cecc173df23d2040819976166e336623
정창원 <8500jcw@gmail.com>
2023-01-29 01:13:40 +0900

    :memo: style: 소셜로그인 과정 설명 주석 추가, 불필요한 주석 삭제

--- src/pages/user/kakaoLogin/SocialAuth.jsx
+++ src/pages/user/kakaoLogin/SocialAuth.jsx
@@ -2,8 +2,18 @@ import React, { useEffect } from "react";
 import axios from "axios";
 import { setCookie } from "../../../shared/cookie";
 
+/****************************************************************************
+ * 4. 해당 페이지의 query에 있는 인증코드를 추출하고 이를 백 서버에 보냄
+ * 5. 백에서 작업을 거쳐서 우리의 서비스에 필요한 token을 발급하여 응답으로 준다.
+ * 6. 프론트 역할 끝 (이하는 그 후 백엔드에서 처리 과정)
+ * 7. 인증 code를 통해 백엔드 서버에서 kakao server에 token을 요청한다.
+ * 8. 받아온 token을 통해서 회원정보를 받아온다.
+ * 9. 받아온 회원 정보를 DB에 저장하고 이 정보로 우리 서비스에 필요한 token을 발행
+ ***************************************************************************/
+
 const Socialauth = () => {
   useEffect(() => {
+    // url에 있는 query추출하는 역할
     const params = new URLSearchParams(window.location.search);
     let code = params.get("code");
 
@@ -15,18 +25,15 @@ const Socialauth = () => {
           {
             code: code,
           },
-          { withCredentials: true }
+          { withCredentials: true } // withCredentials = 로그인 상태를 기억하기 위한 credentials 설정(for Cors)
         )
         .then((res) => {
-          console.log("res: ", res);
           setCookie("auth", res.data.access_token);
           window.location.href = "/";
         })
         .catch((err) => {
           const errorMessage = err.response.data.errorMessage;
-          console.log("errorMessage: ", errorMessage);
           alert(errorMessage);
-          // window.location.href = '/';
         });
     }
   }, []);
--- src/pages/user/kakaoLogin/Start.jsx
+++ src/pages/user/kakaoLogin/Start.jsx
@@ -3,11 +3,18 @@ import styled from "styled-components";
 import kakaobtn from "../../../asset/button/kakaobtn.svg";
 
 const Start = () => {
+  /*****************************************************************
+   * 2. 함수에서 return 하는 kakao server url로 이동
+   * 3. 우리가 설정한 리다이렉트 URL 주소로 이동되면서,
+   *    query로 인증 code를 받아옴 (SocialAuth 컴포넌트로 리다이렉트)
+   * ***************************************************************/
   const devModeOrProductionMode = () => {
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&response_type=code`; // localhost:3000/social?code=sdgaksjghjhgljsdhfg
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URL}&response_type=code`;
     return KAKAO_AUTH_URL;
   };
-
+  /******************************************************
+   * 1. 카카오 로그인 클릭시 devModeOrProductionMode 작동  *
+   * ****************************************************/
   const onClickKakaoLogin = async () => {
     console.log("kakao_login click");
     window.location.href = devModeOrProductionMode();
