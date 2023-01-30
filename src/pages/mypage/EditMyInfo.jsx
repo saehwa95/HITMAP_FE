f9f98c9e1c04a164521e8b0e44fd8c88993cf512
Saehwa95 <tpghk1002@naver.com>
2023-01-30 18:24:50 +0900

    :memo: 충돌 해결

--- src/pages/mypage/EditMyInfo.jsx
+++ src/pages/mypage/EditMyInfo.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import EditMypageAppBar from "../../components/layout/appBar/EditMypageAppBar";
 import UserEdit from "../../components/myPage/EditMyInfo/UserEdit";
-import Logout from "../../components/myPage/EditMyInfo/Logout";
-import Withdraw from "../../components/myPage/EditMyInfo/Withdraw";
 import LogoutWithdraw from "../../components/myPage/EditMyInfo/LogoutWithdraw/LogoutWithdraw";
 
 const EditMyInfo = () => {
@@ -10,8 +8,6 @@ const EditMyInfo = () => {
     <div>
       <EditMypageAppBar />
       <UserEdit />
-      <Logout />
-      <Withdraw />
       <LogoutWithdraw />
     </div>
   );
