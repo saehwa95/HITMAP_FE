192fd2dd8b338701a1e8cd0b43bb407a1e12a118
정창원 <8500jcw@gmail.com>
2023-02-01 18:32:32 +0900

    :memo: fix: 채팅기능 관련 코드 주석처리

--- src/components/socket/ChatCard.jsx
+++ src/components/socket/ChatCard.jsx
@@ -1,106 +1,106 @@
-import React from "react";
-import styled from "styled-components";
+// import React from "react";
+// import styled from "styled-components";
 
-const ChatCard = () => {
-  return (
-    <StChatList>
-      <StOtherPersonProfileImgBox>
-        <StOtherPersonProfileImg
-          alt="상대프로필자리"
-          // src={}
-        />
-      </StOtherPersonProfileImgBox>
-      <StChatInfoBox>
-        <StOtherPersonInfo>
-          <StOtherPersonNickName>상대프로필닉네임</StOtherPersonNickName>
-          <StChatLastCreateTime>17:50</StChatLastCreateTime>
-        </StOtherPersonInfo>
-        <StChatContentInfo>
-          <StChatLastContent>마지막 메시지 자리</StChatLastContent>
-          <StChatStackNumber>2</StChatStackNumber>
-        </StChatContentInfo>
-      </StChatInfoBox>
-    </StChatList>
-  );
-};
+// const ChatCard = () => {
+//   return (
+//     <StChatList>
+//       <StOtherPersonProfileImgBox>
+//         <StOtherPersonProfileImg
+//           alt="상대프로필자리"
+//           // src={}
+//         />
+//       </StOtherPersonProfileImgBox>
+//       <StChatInfoBox>
+//         <StOtherPersonInfo>
+//           <StOtherPersonNickName>상대프로필닉네임</StOtherPersonNickName>
+//           <StChatLastCreateTime>17:50</StChatLastCreateTime>
+//         </StOtherPersonInfo>
+//         <StChatContentInfo>
+//           <StChatLastContent>마지막 메시지 자리</StChatLastContent>
+//           <StChatStackNumber>2</StChatStackNumber>
+//         </StChatContentInfo>
+//       </StChatInfoBox>
+//     </StChatList>
+//   );
+// };
 
-export default ChatCard;
+// export default ChatCard;
 
-const StChatList = styled.div`
-  width: 343px;
-  height: 72px;
-  background: #ffffff;
-  box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.04);
-  margin: 0 16px;
-  border-radius: 16px;
-  display: flex;
-  flex-direction: row;
-`;
+// const StChatList = styled.div`
+//   width: 343px;
+//   height: 72px;
+//   background: #ffffff;
+//   box-shadow: 2px 4px 4px rgba(0, 0, 0, 0.04);
+//   margin: 0 16px;
+//   border-radius: 16px;
+//   display: flex;
+//   flex-direction: row;
+// `;
 
-const StOtherPersonProfileImgBox = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 0 16px;
-`;
-const StOtherPersonProfileImg = styled.img`
-  width: 48px;
-  height: 48px;
-  border-radius: 50px;
-`;
+// const StOtherPersonProfileImgBox = styled.div`
+//   display: flex;
+//   align-items: center;
+//   padding: 0 16px;
+// `;
+// const StOtherPersonProfileImg = styled.img`
+//   width: 48px;
+//   height: 48px;
+//   border-radius: 50px;
+// `;
 
-const StChatInfoBox = styled.div`
-  padding: 12px 0;
-  display: flex;
-  flex-direction: column;
-  width: 248px;
-  gap: 8px;
-`;
+// const StChatInfoBox = styled.div`
+//   padding: 12px 0;
+//   display: flex;
+//   flex-direction: column;
+//   width: 248px;
+//   gap: 8px;
+// `;
 
-const StOtherPersonInfo = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: row;
-`;
+// const StOtherPersonInfo = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: space-between;
+//   flex-direction: row;
+// `;
 
-const StOtherPersonNickName = styled.div`
-  font-weight: 700;
-  font-size: 18px;
-  color: #3f3f3f;
-`;
+// const StOtherPersonNickName = styled.div`
+//   font-weight: 700;
+//   font-size: 18px;
+//   color: #3f3f3f;
+// `;
 
-const StChatLastCreateTime = styled.div`
-  font-weight: 500;
-  font-size: 12px;
-  color: #c2c2c2;
-`;
+// const StChatLastCreateTime = styled.div`
+//   font-weight: 500;
+//   font-size: 12px;
+//   color: #c2c2c2;
+// `;
 
-const StChatContentInfo = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-`;
+// const StChatContentInfo = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   flex-direction: row;
+// `;
 
-const StChatLastContent = styled.div`
-  font-weight: 500;
-  font-size: 14px;
-  color: #979797;
-  //string길어지면 ...으로 처리하는 css
-  width: 180px;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-`;
+// const StChatLastContent = styled.div`
+//   font-weight: 500;
+//   font-size: 14px;
+//   color: #979797;
+//   //string길어지면 ...으로 처리하는 css
+//   width: 180px;
+//   text-overflow: ellipsis;
+//   white-space: nowrap;
+//   overflow: hidden;
+// `;
 
-const StChatStackNumber = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 22px;
-  height: 22px;
-  background: #e5294a;
-  border-radius: 50px;
-  color: white;
-  font-weight: 400;
-  font-size: 10px;
-`;
+// const StChatStackNumber = styled.div`
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   width: 22px;
+//   height: 22px;
+//   background: #e5294a;
+//   border-radius: 50px;
+//   color: white;
+//   font-weight: 400;
+//   font-size: 10px;
+// `;
