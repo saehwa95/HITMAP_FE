9540a8d029e66935c59cb15f17eeecd254090fe5
Saehwa95 <tpghk1002@naver.com>
2023-02-23 10:02:14 +0900

    :sparkles: feat React.lazy 적용

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import SignUp from "../pages/user/SignUp";
-import Login from "../pages/user/Login";
-import SocialAuth from "../pages/user/kakaoLogin/SocialAuth";
-import Main from "../pages/main/Main";
-import Weather from "../pages/main/Weather";
-import PostList from "../pages/sns/PostList";
-import DetailPost from "../pages/sns/DetailPost";
-import CreatePost from "../pages/sns/CreatePost";
-import MyPage from "../pages/mypage/MyPage";
-import EditMyInfo from "../pages/mypage/EditMyInfo";
-import EditPasswordPage from "../pages/mypage/EditPasswordPage";
-import LogInRegister from "../pages/mypage/LogInRegister";
-import Withdraw from "../components/myPage/EditMyInfo/LogoutWithdraw/Withdraw";
-import PrivateRoutes from "./PrivateRoutes";
+
+const SignUp = React.lazy(()=> import("../pages/user/SignUp"));
+const Login = React.lazy(()=> import("../pages/user/Login"));
+const SocialAuth = React.lazy(()=> import("../pages/user/kakaoLogin/SocialAuth"));
+const Main = React.lazy(()=> import("../pages/main/Main"));
+const Weather = React.lazy(()=> import("../pages/main/Weather"));
+const PostList = React.lazy(()=> import("../pages/sns/PostList"));
+const DetailPost = React.lazy(()=> import("../pages/sns/DetailPost"));
+const CreatePost = React.lazy(()=> import("../pages/sns/CreatePost"));
+const MyPage = React.lazy(()=> import("../pages/mypage/MyPage"));
+const EditMyInfo = React.lazy(()=> import("../pages/mypage/EditMyInfo"));
+const EditPasswordPage = React.lazy(()=> import("../pages/mypage/EditPasswordPage"));
+const LogInRegister = React.lazy(()=> import("../pages/mypage/LogInRegister"));
+const Withdraw = React.lazy(()=> import("../components/myPage/EditMyInfo/LogoutWithdraw/Withdraw"));
+const PrivateRoutes = React.lazy(()=> import("./PrivateRoutes"));
 
 const Router = () => {
   return (
     <BrowserRouter>
+    <Suspense>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Login />} />
@@ -35,6 +37,7 @@ const Router = () => {
           <Route path="/withdraw" element={<Withdraw />} />
         </Route>
       </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
